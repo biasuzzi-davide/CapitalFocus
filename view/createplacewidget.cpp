8a0e6c9b4b5ff5e3fe53467b421d28a90dd10038
AlbeUni <alberto.reginato.1@studenti.unipd.it>
2025-05-19 10:26:58 +0200

    Update createplacewidget.cpp

--- view/createplacewidget.cpp
+++ view/createplacewidget.cpp
@@ -1,6 +1,6 @@
 // CreatePlaceWidget.cpp
 #include "view/createplacewidget.h"
-#include "ui_CreatePlaceWidget.h"
+#include "../build/Qt_6_5_3_for_macOS-Debug/ui_CreatePlaceWidget.h"
 #include "controller/placecontroller.h"
 #include <QComboBox>
 #include <QPushButton>
